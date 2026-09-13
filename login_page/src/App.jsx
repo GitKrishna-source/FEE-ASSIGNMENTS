55a81bbc410e4b142e644a87e5880dac805071df
Krishna <krishnaunofficial123@gmail.com>
2026-09-13 18:13:03 +0530

    Fifth project

---
+++ login_page/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./index.css";
+
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("Please fill all fields");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters");
+      return;
+    }
+
+    alert("Login Successful ✅");
+  };
+
+  return (
+    <div className="container">
+      <form className="login-card" onSubmit={handleLogin}>
+        
+        <h1>Login</h1>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+
+      </form>
+    </div>
+  );
+};
+
+export default App;
